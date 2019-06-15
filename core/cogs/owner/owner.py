e6d9783a0c8cbaecee0aa585cd2a26e05d5f6c20
IceCereal <ns_raghav@hotmail.com>
2019-06-15 19:16:01 +0530

    add logout in ownerCog

--- core/cogs/owner/owner.py
+++ core/cogs/owner/owner.py
@@ -15,5 +15,27 @@ class ownerCog(commands.Cog):
 		print ("Loading Cog:\townerCog")
 		self.bot = bot
 
+	@commands.command(
+		name = "logout",
+		brief = "Log the client out",
+		usage = "logout",
+		enabled = True,
+		description = "Log the client out. This is only an owner command.",
+		hidden = True
+	)
+	@commands.is_owner()
+	async def logout(self, ctx):
+		print ("Logout - Initiated")
+		await ctx.channel.send(content="Logout Sequence Initiated")
+
+		#Any clean-up code goes here
+		await self.bot.change_presence(status = discord.Status.offline)
+
+		print ("Logout - Completed")
+		await ctx.channel.send(content="Logout Sequence Completed")
+
+		await sleep(1)
+		await self.bot.logout()
+
 def setup(bot):
 	bot.add_cog(ownerCog(bot))
