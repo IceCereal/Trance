6a6d7543bd455f736bd648473534a7b71c077087
IceCereal <ns_raghav@hotmail.com>
2019-06-25 07:44:44 +0530

    create adminCog

---
+++ core/cogs/admin/admin.py
@@ -0,0 +1,40 @@
+"""
+	A Cog for Admins
+"""
+
+import discord
+from discord.ext import commands
+from asyncio import sleep
+from os import path, makedirs
+from json import dump, load
+from pathlib import Path as PathPL
+
+class adminCog(commands.Cog):
+	def __init__(self, bot):
+		print ("Loading Cog:\tadminCog")
+		self.bot = bot
+
+	@commands.command(
+		name = "logout",
+		brief = "Log the client out",
+		usage = "logout",
+		enabled = True,
+		description = "Log the client out. This is only an owner command.",
+		hidden = True
+	)
+	@commands.has_roles("Admin")
+	async def logout(self, ctx):
+		print ("Logout - Initiated")
+		await ctx.channel.send(content="Logout Sequence Initiated")
+
+		#Any clean-up code goes here
+		await self.bot.change_presence(status = discord.Status.offline)
+
+		print ("Logout - Completed")
+		await ctx.channel.send(content="Logout Sequence Completed")
+
+		await sleep(1)
+		await self.bot.logout()
+
+def setup(bot):
+	bot.add_cog(adminCog(bot))
